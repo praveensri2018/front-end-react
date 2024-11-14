cf3db337cd44a907abb03d64a349114ffb3205f4
Praveen Bala <85008863+praveensri2018@users.noreply.github.com>
2024-11-14 10:04:08 +0530

    logo update
    logo updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// src/App.js
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Welcome to Ecommerce Project's</code>
+        </p>
       </header>
     </div>
   );
